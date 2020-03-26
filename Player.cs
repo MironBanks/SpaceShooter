56b4a529f83a699f671e46613be68bf3a75cebb3
Miron Banks <madapegarage@gmail.com>
2020-03-26 09:03:56 +0100

    first error

--- Player.cs
+++ Player.cs
@@ -7,12 +7,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
